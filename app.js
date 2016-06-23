98a4506c60dda9b350515035eb011da7de0f75dc
PonchoD <tony.bardill@gmail.com>
2016-06-23 11:28:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -20,6 +20,32 @@ var main = function() {
       left: "0px"
     }, 200);
   });
+  
+    $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
+
+  $(document).keypress(function(event) {
+    if(event.which === 111) {
+      $('.description').hide();
+
+      $('.current').children('.description').show();
+    }
+
+    else if(event.which === 110) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+      
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
+    }
+  });
+  
+  
 };
 
 
